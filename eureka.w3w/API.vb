654e6e211cfc9fc4978a6fb24e64dfd1ee5ba938
Damien Power <dpower@eureka.ie>
2025-07-11 14:44:21 +0100

    nuget

--- eureka.w3w/API.vb
+++ eureka.w3w/API.vb
@@ -3,18 +3,17 @@ Imports eureka.w3w.models
 Public Class API
     Public Property Settings As New settings
     Private Function GetClient() As RestClient
-        Dim Client = New RestClient
-        Client.BaseUrl = "https://api.what3words.com/v3/"
+        Dim Client = New RestClient("https://api.what3words.com/v3/")
         Client.AddDefaultParameter("key", Settings.APIKey, ParameterType.QueryString)
         Return Client
     End Function
 
-    Public Function Geocode(ByVal words As String) As Result
+    Public Function Geocode(words As String) As Result
         Dim result As New Result
         Try
             Dim Client = GetClient()
             Dim request As New RestRequest("convert-to-coordinates")
-            request.Method = Method.GET
+            request.Method = Method.Get
             request.AddParameter("words", words, ParameterType.QueryString)
             request.RequestFormat = DataFormat.Json
             Dim Response As RestResponse(Of geocoderesponse) = Client.Execute(Of geocoderesponse)(request)
@@ -26,12 +25,12 @@ Public Class API
         End Try
         Return result
     End Function
-    Public Function ReverseGeocode(ByVal coordinates As coordinates) As Result
+    Public Function ReverseGeocode(coordinates As coordinates) As Result
         Dim result As New Result
         Try
             Dim Client = GetClient()
             Dim request As New RestRequest("convert-to-3wa")
-            request.Method = Method.GET
+            request.Method = Method.Get
             request.AddParameter("coordinates", String.Format("{0},{1}", coordinates.lat, coordinates.lng), ParameterType.QueryString)
             request.RequestFormat = DataFormat.Json
             Dim Response As RestResponse(Of geocoderesponse) = Client.Execute(Of geocoderesponse)(request)
